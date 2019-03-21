5ce6cdd45e2d610148e43421765a4f5fbaba399b
Mami Taniguro <48035737+MamiTaniguro@users.noreply.github.com>
2019-03-21 13:25:16 -0700

    Update time_to_study.py

--- time_to_study/time_to_study.py
+++ time_to_study/time_to_study.py
@@ -1,3 +1,6 @@
+# This is a python mini project to remind a user that "it's time to study!" 
+# Once a user runs the program, it keeps tracking time and opens the browser to play learning videos.
+
 import webbrowser
 import time
 
